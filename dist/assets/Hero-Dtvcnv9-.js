b37a11e33cafb9238071276d26dd92259f2f6e9a
Naincy garg <113368930+123naincy@users.noreply.github.com>
2026-01-15 13:06:09 +0530

    feat: add custom navigation component for Swiper with SVG icons and enhanced functionality

--- dist/assets/Hero-Dtvcnv9-.js
+++ dist/assets/Hero-Dtvcnv9-.js
@@ -1,4 +1,4 @@
-import{c as o,u as a,r,j as e}from"./index-o0JNAVCe.js";/**
+import{c as o,u as a,r,j as e}from"./index-tfvnf5kt.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
